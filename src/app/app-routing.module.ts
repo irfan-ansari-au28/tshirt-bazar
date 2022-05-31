e3b2ef39849eb478486876b009dae7a52dbf3854
Irfan-Ansari-au28 <irfan398649@gmaiil.com>
2022-05-31 17:37:11 +0530

    implement routing for header links

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './component/home/home.component';
+import { MenComponent } from './component/men/men.component';
+import { OfferComponent } from './component/offer/offer.component';
+import { WomenComponent } from './component/women/women.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'men', component: MenComponent },
+  { path: 'women', component: WomenComponent },
+  { path: 'offer', component: OfferComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './component/navbar/top-bar/top-bar.component';
 import { HeaderComponent } from './component/navbar/header/header.component';
+import { HomeComponent } from './component/home/home.component';
+import { MenComponent } from './component/men/men.component';
+import { WomenComponent } from './component/women/women.component';
+import { OfferComponent } from './component/offer/offer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TopBarComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent,
+    MenComponent,
+    WomenComponent,
+    OfferComponent
   ],
   imports: [
     BrowserModule,

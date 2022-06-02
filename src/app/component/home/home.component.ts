358cb21c390b9f4d406ae5cf9579b5d18aa25a51
Irfan-Ansari-au28 <irfan398649@gmaiil.com>
2022-06-02 18:15:52 +0530

    create product interface and service

--- src/app/component/home/home.component.ts
+++ src/app/component/home/home.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductsService } from 'src/app/service/products.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  constructor(private productsService: ProductsService) {}
 
   ngOnInit(): void {
+    console.log(this.productsService.products);
   }
-
 }
